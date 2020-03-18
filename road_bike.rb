d87a7bc8b6a61b2cee4d1ab39e66964d2318f5a2
MaxTheMitchell <rat2043@gmail.com>
2020-03-18 11:20:44 -0700

    add total price method to road bike
    refrences #1

--- road_bike.rb
+++ road_bike.rb
@@ -7,12 +7,20 @@ class RoadBike
     @daily_rate = 15
   end
 
+  def total_price(days,hours)
+    total_pannier_price + daily_rate*days
+  end
+
   def clean
     puts "Cleaning..."
   end
 
   def lubricate_gears
     puts "Lubricating gears..."
+  end
+
+  def total_pannier_price
+    panniers.reduce(0){|t,p|t+p.price}
   end
 
   def panniers
